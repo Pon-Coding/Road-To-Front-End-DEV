428d3e7f10776b50fe2a2ed08d1c73decda65a38
HCM_IMAC_02 <>
2023-11-07 16:40:42 +0700

    no message

--- JavaScript/01. Basics/basics.js
+++ JavaScript/01. Basics/basics.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+console.log("Hello Blue Technology");
+
+var counter = 0;
+var fontSize = 30;
+function ClickMe(){
+    counter++;
+    document.getElementById('InputMe').innerHTML = "BLUE TECHNOLOGY --> "+counter;
+    
+}
+
+function chnageColor(){
+    document.getElementById('InputMe').style.color = "BLUE";
+}
+function resetColor(){
+    document.getElementById('InputMe').style.color = "BLACK";
+}
+
+function loopIncreaseFont(){
+    fontSize+=3;
+    document.getElementById('InputMe').style.fontSize = fontSize+"px";
+}
